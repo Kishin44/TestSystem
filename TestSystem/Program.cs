3a337661be8b04cb859b295984587a695342ff44
Kishin44 <stef_stoj4ev@abv.bg>
2014-11-14 21:15:57 +0200

    Version 1 test.

--- TestSystem/Program.cs
+++ TestSystem/Program.cs
@@ -16,7 +16,7 @@ namespace TestSystem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainFrom());
         }
     }
 }
